389f0dfc3519133afddeb4db32c855c05360431c
aubertainstain <160878259+aubertainstain@users.noreply.github.com>
2024-04-08 20:30:41 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,6 +1,3 @@
-        var numero1 = parseFloat(prompt("Digite o primeiro número:"));
-        var numero2 = parseFloat(prompt("Digite o segundo número:"));
-
-        var soma = numero1 + numero2;
-
-        document.write("A soma de " + numero1 + " e " + numero2 + " é igual a " + soma);
+const num1 = parseFloat(prompt("Digite o primeiro número: "));
+const num2 = parseFloat(prompt("Digite o segundo número: "));
+console.log("A soma é:", num1 + num2);
